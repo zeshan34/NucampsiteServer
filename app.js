ec7cdc5feca5891ff70b228a44b53d96f0b1de77
shaikhzeshan@gmail.com <shaikhzeshan@gmail.com>
2020-08-15 02:13:33 -0500

    express rest api with mongoDB and mongoose part 1

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ var promotions = require('./routes/promotionRouter');
 var partnerRouter = require('./routes/partnerRouter');
 var campsiterouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
 
 var app = express();
 
