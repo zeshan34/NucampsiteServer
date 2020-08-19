cd71727cd776316c06e2e504f662db4172d4e64f
shaikhzeshan@gmail.com <shaikhzeshan@gmail.com>
2020-08-18 22:38:52 -0500

    session part 3

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose =require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { 
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    admin:{
+        type: Boolean,
+        default: false
+    }
+});
+
+    module.exports = mongoose.model('User',userSchema);
+
